83a9f859b33b4c9303938981c727e3ad48e9c1ad
Iapetus-11 <olimdahacker@gmail.com>
2020-06-07 17:24:17 -0400

    update player rank test

--- tests/player_rank.py
+++ tests/player_rank.py
@@ -3,7 +3,7 @@ import aiopypixel
 
 
 async def rank():
-    api = aiopypixel.Client("a8965b71-df11-468e-8be2-9ec64e56adf0")
+    api = aiopypixel.Client(["a8965b71-df11-468e-8be2-9ec64e56adf0"])
     await api.getRank("Iapetus11")
 
 
