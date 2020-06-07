9675de78dd30ead992c430da2c2cb8a30ba4a137
Iapetus-11 <olimdahacker@gmail.com>
2020-06-07 17:22:27 -0400

    update player rank test

--- tests/player_rank.py
+++ tests/player_rank.py
@@ -1,9 +1,11 @@
 import asyncio
-from aiopypixel import Client
+
+import aiopypixel
 
 
 async def rank():
-    api = Client('a8965b71-df11-468e-8be2-9ec64e56adf0')
-    await api.getRank('TrustedMercury')
+    api = aiopypixel.Client("a8965b71-df11-468e-8be2-9ec64e56adf0")
+    await api.getRank("Iapetus11")
+
 
 asyncio.get_event_loop().run_until_complete(rank())
