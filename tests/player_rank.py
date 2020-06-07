22ceab3ad6f1ae7cb1b37370b8a213ffd024da5d
Iapetus-11 <olimdahacker@gmail.com>
2020-06-07 17:43:24 -0400

    client.close()

--- tests/player_rank.py
+++ tests/player_rank.py
@@ -5,6 +5,7 @@ import aiopypixel
 async def rank():
     client = aiopypixel.Client("a8965b71-df11-468e-8be2-9ec64e56adf0")
     await client.getRank("Iapetus11")
+    await client.close()
 
 
 asyncio.get_event_loop().run_until_complete(rank())
