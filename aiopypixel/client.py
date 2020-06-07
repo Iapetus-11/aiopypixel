16d69ce86c2c575033cb3379900f04862fcd5c19
Iapetus-11 <olimdahacker@gmail.com>
2020-06-07 17:04:28 -0400

    helper function/method for get requests

--- aiopypixel/client.py
+++ aiopypixel/client.py
@@ -1,16 +1,37 @@
-import asyncio
 import aiohttp
+import asyncio
+from random import choice
 
 
 class Client:
 
-    def __init__(self, api_key):
-        self.API_KEY = api_key
+    def __init__(self, api_keys):
+        # Handle using a singular key
+        if not isinstance(api_keys, list):
+            api_keys = [api_keys]
+
+        self.API_KEYS = api_keys
+
         self.BASE_URL = 'https://api.hypixel.net'
+
         self.session = aiohttp.ClientSession()
 
-    async def getRank(self, player):
+    async def close(self):
+        # CLEANUP HERE
+        await self.session.close()
 
-        data = await self.session.get(f'{self.BASE_URL}/player?key={self.API_KEY}&name={player}')
+    async def get(self, url):
+        response = await self.session.get(url.replace("api_key", choice(self.API_KEYS)))
+        failed = 0
+        while response.status == 429 and failed <= 50:
+            await asyncio.sleep(.5)
+            response = await self.session.get(url.replace("api_key", choice(self.API_KEYS)))
+        if failed > 50:
+            # RAISE AN EXCEPTION HERE!
+            pass
+        return response
+
+    async def getRank(self, player):
+        data = await self.session.get(f'{self.BASE_URL}/player?key=api_key&name={player}')
         data = await data.json()
         print(data)
