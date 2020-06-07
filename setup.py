58206ee261df2faaa9ff3702345fe2080099fb17
TrustedMercury <kevin.jt2007@gmail.com>
2020-06-07 16:15:44 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read(s)
+
+setuptools.setup(
+    name="aiopypixel",
+    version="0.1",
+    author="TrustedMercury & Iapetus11",
+    description="An asynchronous python wrapper for the Hypixel API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Villager-Dev/aiopypixel",
+    packages=setuptools.find_packages(),
+    classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
