7b08c395aaa90c10e2664efe142081d7502573a3
Iapetus-11 <olimdahacker@gmail.com>
2020-06-08 15:16:31 -0400

    xp -> exp

--- aiopypixel/models/player.py
+++ aiopypixel/models/player.py
@@ -1,13 +1,14 @@
 class Player:
-    def __init__(self, uuid, hypixel_id, display_name, first_login, last_login, last_logout, xp, stats, achievements,
+    def __init__(self, uuid, hypixel_id, display_name, first_login, last_login, last_logout, exp, stats, achievements,
                  one_time_achievements):
+        """A base class for a hypixel player that contains only the most important data"""
         self.UUID = uuid
         self.HYPIXEL_ID = hypixel_id
         self.DISPLAY_NAME = display_name
         self.FIRST_LOGIN = first_login
         self.LAST_LOGIN = last_login
         self.LAST_LOGOUT = last_logout
-        self.XP = xp
+        self.EXP = exp
         self.STATS = stats
         self.ACHIEVEMENTS = achievements
         self.ONE_TIME_ACHIEVEMENTS = one_time_achievements
