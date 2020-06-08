dc00b138a0fcb4c1b3bf04274348d9abf2ef0157
TrustedMercury <kevin.jt2007@gmail.com>
2020-06-07 21:07:55 -0400

    fixed some stuff

--- aiopypixel/client.py
+++ aiopypixel/client.py
@@ -1,12 +1,13 @@
 import aiohttp
 import asyncio
-from random import choice
+import random
 
 
 class Client:
 
     def __init__(self, api_keys):
-        # Handle using a singular key
+
+        # Handles the instance of a singular key
         if not isinstance(api_keys, list):
             api_keys = [api_keys]
 
@@ -16,39 +17,50 @@ class Client:
 
         self.session = aiohttp.ClientSession()
 
-    async def close(self):
-        # Do any cleanup here ig
+    async def exit(self):
+
+        # Safe client cleanup and exit
         await self.session.close()
 
-    async def get(self, url):
-        response = await self.session.get(url.replace("api_key", choice(self.API_KEYS)))
-        failed = 0
-        while response.status == 429 and failed <= 121:  # 121 to give just a little bit of extra leeway
-            await asyncio.sleep(.5)
-            response = await self.session.get(url.replace("api_key", choice(self.API_KEYS)))
+    async def get(self, url: str) -> dict:
 
-        if failed > 120:
-            # RAISE AN EXCEPTION HERE!
-            pass
+        """base method to fetch api response"""
+
+        response = await self.session.get(url.replace("api_key", random.choice(self.API_KEYS)))
+
+        if response.status == 429:
+            pass  # raise errors here
 
         return await response.json()
 
     # Returns list of the uuids for the players who are friends with the specified player
     # Returns an empty list if the user is not found.
-    async def getFriends(self, player_uuid):
-        data = await self.get(f"{self.BASE_URL}/friends?key=api_key&uuid={player_uuid}")
-        if not data["success"]:  # If player can't be found or status is failed or somethin idk
-            return []
+    async def getFriends(self, player) -> list:
+
+        """returns the friends list of the provided player (list of uuids)
+        if the user doesn't have any friends, returns an empty list"""
+
+        if len(player) < 16:
+            pass  # replace name with uuid
+
+        data = await self.get(f"{self.BASE_URL}/friends?key=api_key&uuid={player}")
+
+        if not data["success"]:
+            pass  # raise error later
+
         uuids = []
+
         for record in data["records"]:
-            if record["uuidSender"] != player_uuid:
+            if record["uuidSender"] != player:
                 uuids.append(record["uuidSender"])
             else:
                 uuids.append(record["uuidReceiver"])
         return uuids
 
     async def getGuildFromID(self, guild_id):
+
         data = await self.get(f"{self.BASE_URL}/friends?key=api_key&id={guild_id}")
 
     async def getRank(self, player):
+
         return await self.get(f"{self.BASE_URL}/player?key=api_key&name={player}")
