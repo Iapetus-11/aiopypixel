9da123dff00e130cfae495c6817cf1feb7f0343e
Iapetus-11 <olimdahacker@gmail.com>
2020-06-08 01:13:49 -0400

    friends and guild stuff

--- aiopypixel/client.py
+++ aiopypixel/client.py
@@ -5,8 +5,8 @@ import random
 
 
 class RateLimited(Exception):
-    def __init__(self, message="[429] You're being rate limited!"):
-        self.message = message
+    def __init__(self, specifics, message="You're being rate limited! ({0})"):
+        self.message = message.format(specifics)
         super().__init__(self.message)
 
     def __str__(self):
@@ -22,6 +22,15 @@ class PlayerNotFound(Exception):
         return self.message
 
 
+class NotFound(Exception):
+    def __init__(self, specifics, message="{0} couldn't be found!"):
+        self.message = message.format(specifics)
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
+
+
 class Client:
     def __init__(self, api_keys):
         # Handles the instance of a singular key
@@ -30,7 +39,7 @@ class Client:
 
         self.API_KEYS = api_keys
 
-        self.BASE_URL = 'https://api.hypixel.net'
+        self.BASE_URL = 'https://api.hypixel.net/'
 
         self.session = aiohttp.ClientSession()
 
@@ -41,15 +50,16 @@ class Client:
     async def get(self, url: str) -> dict:
         """base method to fetch api response"""
 
-        response = await self.session.get(url.replace("api_key", random.choice(self.API_KEYS)))
+        response = await self.session.get(f"{self.BASE_URL}" + url.replace("api_key", random.choice(self.API_KEYS)))
 
         if response.status == 429:
-            raise RateLimited
+            raise RateLimited("Hypixel")
 
         return await response.json()
 
     async def GamertagToUUID(self, gamertag) -> str:
         """takes in an mc gamertag and tries to convert it to a mc uuid"""
+
         data = await self.session.get("https://api.mojang.com/profiles/minecraft", json=[gamertag])
         data = await data.json()
 
@@ -60,6 +70,7 @@ class Client:
 
     async def UUIDToGamertag(self, uuid) -> str:
         """takes in an mc uuid and converts it to an mc gamertag"""
+
         data = await self.session.get(f"https://api.mojang.com/user/profiles/{uuid}/names")
 
         if data.status == 204:
@@ -67,16 +78,17 @@ class Client:
 
         return (await data.json())[len(j) - 1]["name"]
 
-    async def getFriends(self, player) -> list:
+    async def getPlayerFriends(self, player) -> list:
         """returns the friends list of the provided player (list of uuids)
         if the user doesn't have any friends, returns an empty list"""
 
-        if len(player) < 16:
+        if len(player) < 17:
+            player = await self.GamertagToUUID(player)
 
-        data = await self.get(f"{self.BASE_URL}/friends?key=api_key&uuid={player}")
+        data = await self.get(f"friends?key=api_key&uuid={player}")
 
         if not data["success"]:
-            return False  # raise error later
+            raise PlayerNotFound("Error while getting player friends!")
 
         uuids = []
 
@@ -87,8 +99,48 @@ class Client:
                 uuids.append(record["uuidReceiver"])
         return uuids
 
-    async def getGuildFromID(self, guild_id):
-        data = await self.get(f"{self.BASE_URL}/friends?key=api_key&id={guild_id}")
+    async def getPlayerGuild(self, player) -> str:
+        """returns the guild id (if any) of that which the provided player is in"""
+
+        if len(player) < 17:
+            player = await self.GamertagToUUID(player)
+
+        data = await self.get(f"findGuild?key=api_key&byUuid={player}")
+
+        if not data["success"]:
+            raise PlayerNotFound("Error while getting player guild!")
+
+        return data["guild"]
+
+    async def getGuildID(self, guild_name):
+        """fetches a hypixel guild id based on the given guild name"""
+
+        data = await self.get(f"guild?key=api_key&name={guild_name}")
+
+        if not data["success"]:
+            raise NotFound("Guild")
+
+        return data["guild"]["_id"]
+
+    async def getGuildName(self, guild_id):
+        """fetches a hypixel guild name based on the given guild id"""
+
+        data = await self.get(f"guild?key=api_key&name={guild_id}")
+
+        if not data["success"]:
+            raise NotFound("Guild")
+
+        return data["guild"]["_id"]
+
+    async def getGuildData(self, guild_id):
+        """fetches a hypixel guild based on the given guild id"""
+
+        data = await self.get(f"guild?key=api_key&name={guild_id}")
+
+        if not data["success"]:
+            raise NotFound("Guild")
+
+        return data["guild"]
 
     async def getRank(self, player):
-        return await self.get(f"{self.BASE_URL}/player?key=api_key&name={player}")
+        return await self.get(f"player?key=api_key&name={player}")
