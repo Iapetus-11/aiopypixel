07c63ce1455c9e22da6bda71803743599780992b
TrustedMercury <kevin.jt2007@gmail.com>
2020-06-08 15:00:53 -0400

    Guild Model

---
+++ aiopypixel/models/guild.py
@@ -0,0 +1,13 @@
+
+class Guild:
+
+    def __init__(self, name, coins, created, exp, description, preferredGames, tag, players):
+
+        self.name = name
+        self.coins = coins
+        self.created = created
+        self.exp = exp
+        self.description = description
+        self.preferredGames = preferredGames
+        self.tag = tag
+        self.players = players
