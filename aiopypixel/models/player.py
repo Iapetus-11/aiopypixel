71369eaf482895a8442a40a6c7ec744d38ce450d
Iapetus-11 <olimdahacker@gmail.com>
2020-06-08 14:57:56 -0400

    add onetime achievements

--- aiopypixel/models/player.py
+++ aiopypixel/models/player.py
@@ -1,10 +1,12 @@
 class Player:
-    def __init__(self, uuid, hypixel_id, display_name, first_login, last_login, xp, achievements, stats):
+    def __init__(self, uuid, hypixel_id, display_name, first_login, last_login, xp, stats, achievements,
+                 one_time_achievements):
         self.UUID = uuid
         self.HYPIXEL_ID = hypixel_id
         self.DISPLAY_NAME = display_name
         self.FIRST_LOGIN = first_login
         self.LAST_LOGIN = last_login
         self.XP = xp
-        self.ACHIEVEMENTS = achievements
         self.STATS = stats
+        self.ACHIEVEMENTS = achievements
+        self.ONE_TIME_ACHIEVEMENTS = one_time_achievements
