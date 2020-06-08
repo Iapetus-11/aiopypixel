fea77dd94e00bb5a57b0bfd514c9791e8caf69a8
TrustedMercury <kevin.jt2007@gmail.com>
2020-06-08 09:57:06 -0400

    Added InvalidPlayerError

--- aiopypixel/exceptions/exceptions.py
+++ aiopypixel/exceptions/exceptions.py
@@ -1,11 +1,13 @@
 
 class Error(Exception):
-
     """base class for other exceptions"""
     pass
 
 
 class RateLimitError(Error):
-
     """Raised when a api ratelimit is reached with the provided key"""
+    pass
+
+class InvalidPlayerError(Error):
+    """Raised when an invalid player name or uuid is provided"""
     pass
