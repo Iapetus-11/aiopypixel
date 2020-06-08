f1027323ec898354f9c1c2f78641b479641dd53c
Iapetus-11 <olimdahacker@gmail.com>
2020-06-08 14:41:04 -0400

    player stuffs

--- aiopypixel/models/player.py
+++ aiopypixel/models/player.py
@@ -1,3 +1,9 @@
 class Player:
-    def __init__(self, uuid):
+    def __init__(self, uuid, hypixel_id, display_name, first_login, last_login, xp, achievements):
         self.UUID = uuid
+        self.HYPIXEL_ID = hypixel_id
+        self.DISPLAY_NAME = display_name
+        self.FIRST_LOGIN = first_login
+        self.LAST_LOGIN = last_login
+        self.XP = xp
+        self.ACHIEVEMENTS = achievements
