ae8ccd99c429f41cadde3312cb7f3e8e95cbeb65
Iapetus-11 <olimdahacker@gmail.com>
2020-06-07 23:53:29 -0400

    helper methods

--- aiopypixel/client.py
+++ aiopypixel/client.py
@@ -4,10 +4,26 @@ import asyncio
 import random
 
 
+class RateLimited(Exception):
+    def __init__(self, message="[429] You're being rate limited!"):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
+
+
+class PlayerNotFound(Exception):
+    def __init__(self, specifics, message="That player couldn't be found! ({0})"):
+        self.message = message.format(specifics)
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
+
+
 class Client:
-
     def __init__(self, api_keys):
-
         # Handles the instance of a singular key
         if not isinstance(api_keys, list):
             api_keys = [api_keys]
@@ -18,31 +34,44 @@ class Client:
 
         self.session = aiohttp.ClientSession()
 
-    async def exit(self):
-
+    async def close(self):
         # Safe client cleanup and exit
         await self.session.close()
 
     async def get(self, url: str) -> dict:
-
         """base method to fetch api response"""
 
         response = await self.session.get(url.replace("api_key", random.choice(self.API_KEYS)))
 
         if response.status == 429:
-            pass  # raise errors here
+            raise RateLimited
 
         return await response.json()
 
-    # Returns list of the uuids for the players who are friends with the specified player
-    # Returns an empty list if the user is not found.
-    async def getFriends(self, player) -> Union[bool, list]:
+    async def GamertagToUUID(self, gamertag) -> str:
+        """takes in an mc gamertag and tries to convert it to a mc uuid"""
+        data = await self.session.get("https://api.mojang.com/profiles/minecraft", json=[gamertag])
+        data = await data.json()
 
+        if not data:
+            raise PlayerNotFound("Error while converting gamertag to uuid!")
+
+        return j[0]["id"]
+
+    async def UUIDToGamertag(self, uuid) -> str:
+        """takes in an mc uuid and converts it to an mc gamertag"""
+        data = await self.session.get(f"https://api.mojang.com/user/profiles/{uuid}/names")
+
+        if data.status == 204:
+            raise PlayerNotFound("Error while converting uuid to gamertag!")
+
+        return (await data.json())[len(j) - 1]["name"]
+
+    async def getFriends(self, player) -> list:
         """returns the friends list of the provided player (list of uuids)
         if the user doesn't have any friends, returns an empty list"""
 
         if len(player) < 16:
-            pass  # replace name with uuid
 
         data = await self.get(f"{self.BASE_URL}/friends?key=api_key&uuid={player}")
 
@@ -59,9 +88,7 @@ class Client:
         return uuids
 
     async def getGuildFromID(self, guild_id):
-
         data = await self.get(f"{self.BASE_URL}/friends?key=api_key&id={guild_id}")
 
     async def getRank(self, player):
-
         return await self.get(f"{self.BASE_URL}/player?key=api_key&name={player}")
