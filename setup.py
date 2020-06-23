ffb0a7ca207192758e421ecf2e16cf71be00e78d
Iapetus-11 <olimdahacker@gmail.com>
2020-06-22 21:03:39 -0400

    debug

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aiopypixel",
-    version="1.7.3",
+    version="1.7.4",
     author="TrustedMercury & Iapetus11",
     description="An asynchronous python wrapper for the Hypixel API.",
     long_description=long_description,
