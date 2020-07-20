14b4de90275570604cdf1646aeaaeae0d14d1ea2
Iapetus-11 <olimdahacker@gmail.com>
2020-07-20 13:44:07 -0400

    1.7.8 DEBUG

--- aiopypixel/client.py
+++ aiopypixel/client.py
@@ -34,6 +34,10 @@ class Client:
         if response.status == 429:
             raise RateLimitError("Hypixel")
 
+        print(response.status)
+        print(response.reason)
+        print(response.headers)
+
         return await response.json()
 
     async def usernameToUUID(self, username: str) -> str:
